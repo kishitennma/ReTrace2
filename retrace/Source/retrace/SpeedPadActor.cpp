6ef16bdcca0abfcf5684128854ca605fda3ee94d
MSI\GA2B <h1202419012@hiratagakuen.onmicrosoft.com>
2025-11-27 16:32:46 +0900

    no message

---
+++ retrace/Source/retrace/SpeedPadActor.cpp
@@ -0,0 +1,85 @@
+#include "SpeedPadActor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/ArrowComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ASpeedPadActor::ASpeedPadActor()
+{
+    PrimaryActorTick.bCanEverTick = true;
+    SetActorTickEnabled(false);
+
+    Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+    RootComponent = Mesh;
+    Mesh->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    Mesh->SetCollisionResponseToAllChannels(ECR_Overlap);
+
+    Mesh->OnComponentBeginOverlap.AddDynamic(this, &ASpeedPadActor::OnOverlapBegin);
+    Mesh->OnComponentEndOverlap.AddDynamic(this, &ASpeedPadActor::OnOverlapEnd);
+
+    DirectionArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("DirectionArrow"));
+    DirectionArrow->SetupAttachment(Mesh);
+    DirectionArrow->ArrowSize = 2.f;
+    DirectionArrow->bIsEditorOnly = true;
+}
+
+void ASpeedPadActor::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void ASpeedPadActor::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    if (!OverlappingPlayer) return;
+
+    FVector FloorDir = DirectionArrow->GetForwardVector().GetSafeNormal();
+    FVector PlayerInput = OverlappingPlayer->GetLastMovementInputVector();
+
+    FVector CurrentVel = OverlappingPlayer->GetVelocity();
+
+    // ベース速度は常に FloorDir 方向
+    FVector TargetVel = FloorDir * BaseSpeed;
+
+    // 正方向入力があれば加速
+    if (!PlayerInput.IsNearlyZero())
+    {
+        float Dot = FVector::DotProduct(PlayerInput.GetSafeNormal(), FloorDir);
+        if (Dot > 0.f)
+        {
+            TargetVel += FloorDir * AcceleratePower * Dot;
+        }
+    }
+
+    // 逆走不可なので Dot <=0 は無視
+
+    // Velocity に反映（Z は保持）
+    FVector NewVel = FVector(TargetVel.X, TargetVel.Y, CurrentVel.Z);
+    OverlappingPlayer->GetCharacterMovement()->Velocity = NewVel;
+}
+
+
+void ASpeedPadActor::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+    bool bFromSweep, const FHitResult& SweepResult)
+{
+    ACharacter* Player = Cast<ACharacter>(OtherActor);
+    if (!Player) return;
+
+    OverlappingPlayer = Player;
+    SetActorTickEnabled(true);
+}
+
+void ASpeedPadActor::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    ACharacter* Player = Cast<ACharacter>(OtherActor);
+    if (!Player) return;
+
+    if (OverlappingPlayer == Player)
+    {
+        OverlappingPlayer = nullptr;
+        SetActorTickEnabled(false);
+    }
+}
---
+++ retrace/Source/retrace/SpeedPadActor.h
@@ -0,0 +1,51 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpeedPadActor.generated.h"
+
+UCLASS()
+class RETRACE_API ASpeedPadActor : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ASpeedPadActor();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void Tick(float DeltaTime) override;
+
+    // Overlap
+    UFUNCTION()
+    void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+        bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:
+    // メッシュ
+    UPROPERTY(VisibleAnywhere)
+    UStaticMeshComponent* Mesh;
+
+    // 加速方向アロー
+    UPROPERTY(VisibleAnywhere)
+    class UArrowComponent* DirectionArrow;
+
+    // プレイヤー参照
+    UPROPERTY()
+    class ACharacter* OverlappingPlayer = nullptr;
+
+    // 加速量
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float AcceleratePower = 2000.f;
+
+    // 勝手に進む床速度
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float BaseSpeed = 600.f;
+};
